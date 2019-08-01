b858fc9f026b4c7148e6fc5c8875fac5e33287dd
Hardik dadhich <hdhadich@in.ibm.com>
2019-08-01 12:28:01 +0530

    adding changes

--- work-folder/Coupledial/relationManager/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
-from django.views.generic import TemplateView
-# Create your views here.
-class Homepage(TemplateView):
-	#return render(request,'homePage.html', None)
-	template_name = 'base.html'
-
-
